1e05e55183cadd339e5d9db21fb122c3211ff1c7
EveNyarango <evenyarango@gmail.com>
2020-12-11 21:31:22 +0300

    create tables

--- create.sql
+++ create.sql
@@ -1,2 +1,31 @@
-SET MODE PostgresSQL;
+--SET MODE PostgresSQL;
+--
+--CREATE DATABASE departments;
+--
+--\c departments
+--
+--
+--
+--CREATE TABLE departments(
+--id SERIAL PRIMARY KEY,
+--departmentname VARCHAR,
+--departmentdescription VARCHAR,
+--noofemployees INT
+--);
+--
+--CREATE TABLE users(
+--id SERIAL PRIMARY KEY,
+--username VARCHAR,
+--userposition VARCHAR,
+--userrole VARCHAR,
+--departmentname VARCHAR
+--);
+--
+--CREATE TABLE news(
+--id SERIAL PRIMARY KEY,
+--title VARCHAR,
+--news VARCHAR,
+--author VARCHAR,
+--departmentname VARCHAR
+--);
 
--- src/main/java/dao/DepartmentsDao.java
+++ src/main/java/dao/DepartmentsDao.java
@@ -1,4 +1,15 @@
 package dao;
 
+import models.Departments;
+
+import java.util.List;
+
 public interface DepartmentsDao {
+
+//Create
+void add(Departments department);
+
+//Read
+List<Departments> getAllDepartments();
+
 }
