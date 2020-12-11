edf10ec3945df3e1ba3ec3b384942889ebf3d6ec
EveNyarango <evenyarango@gmail.com>
2020-12-12 02:15:33 +0300

    add functionality in sql2odepartmentnewsDao

--- src/main/java/dao/Sql2oDepartmentNewsDao.java
+++ src/main/java/dao/Sql2oDepartmentNewsDao.java
@@ -1,4 +1,60 @@
 package dao;
 
-public class Sql2oDepartmentNewsDao {
+import models.DepartmentNews;
+import org.sql2o.Sql2o;
+
+import org.sql2o.Connection;
+import org.sql2o.Sql2o;
+import org.sql2o.Sql2oException;
+import java.util.List;
+
+public class Sql2oDepartmentNewsDao implements DepartmentNewsDao {
+    private Sql2o sql2o;
+
+    public Sql2oDepartmentNewsDao (Sql2o sql2o){
+        this.sql2o = sql2o;
+    }
+
+    @Override
+    public void add(DepartmentNews departmentNews) {
+        try (Connection con = sql2o.open()){
+            String sql = "INSERT INTO news (title, news, author, departmentId) VALUES (:title, :news, :author, :departmentId)";
+            int id = (int) con.createQuery(sql)
+                    .throwOnMappingFailure(false)
+                    .bind(departmentNews)
+                    .executeUpdate()
+                    .getKey();
+            departmentNews.setId(id);
+
+        }
+
+    }
+
+    @Override
+    public List<DepartmentNews> getAllDepartmentNews() {
+        String sql = "SELECT * FROM news WHERE departmentId <> ''";
+        try (Connection con = sql2o.open()){
+            return con.createQuery(sql, true)
+                    .throwOnMappingFailure(false)
+                    .executeAndFetch(DepartmentNews.class);
+        }catch (Sql2oException ex){
+            System.out.println(ex);
+            return null;
+        }
+    }
+
+    @Override
+    public DepartmentNews getDepartmentNewsById(int id) {
+        String sql = "SELECT * FROM news WHERE departmentId <> ''";
+        try (Connection conn = sql2o.open()){
+            return   conn.createQuery(sql)
+                    .throwOnMappingFailure(false)
+                    .addParameter("id", id)
+                    .executeAndFetchFirst(DepartmentNews.class);
+        }catch (Sql2oException ex){
+            System.out.println(ex);
+            return null;
+        }
+    }
+
 }
