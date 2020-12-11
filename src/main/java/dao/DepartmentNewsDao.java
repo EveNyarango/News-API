c0c7eceaaabdc1153f3a51e595c783ccfe4ef9e1
EveNyarango <evenyarango@gmail.com>
2020-12-11 22:06:18 +0300

    add news interface

--- src/main/java/dao/DepartmentNewsDao.java
+++ src/main/java/dao/DepartmentNewsDao.java
@@ -1,4 +1,17 @@
 package dao;
 
+import models.DepartmentNews;
+
+
+import java.util.List;
+
 public interface DepartmentNewsDao {
+//    Create
+    void add (DepartmentNews departmentNews);
+
+//    Read
+    List<DepartmentNews> getAllDepartmentNews();
+
+    DepartmentNews getDepartmentNewsById(int id);
+
 }
--- src/main/java/dao/GeneralNewsDao.java
+++ src/main/java/dao/GeneralNewsDao.java
@@ -1,4 +1,15 @@
 package dao;
 
+import models.GeneralNews;
+
+import java.util.List;
+
 public interface GeneralNewsDao {
+//    create
+    void add (GeneralNews generalNews);
+
+//    Read
+    List<GeneralNews> getAllGeneralNews();
+
+    GeneralNews getGeneralNewsById(int id);
 }
