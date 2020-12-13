40bdd7ea7bba911e9771621ed78a921f21e36b82
EveNyarango <evenyarango@gmail.com>
2020-12-13 16:01:20 +0300

    work on app.java

--- create.sql
+++ create.sql
@@ -1,31 +1,31 @@
---SET MODE PostgresSQL;
---
---CREATE DATABASE departments;
---
---\c departments
---
---
---
---CREATE TABLE departments(
---id SERIAL PRIMARY KEY,
---departmentname VARCHAR,
---departmentdescription VARCHAR,
---noofemployees INT
---);
---
---CREATE TABLE users(
---id SERIAL PRIMARY KEY,
---username VARCHAR,
---userposition VARCHAR,
---userrole VARCHAR,
---departmentname VARCHAR
---);
---
---CREATE TABLE news(
---id SERIAL PRIMARY KEY,
---title VARCHAR,
---news VARCHAR,
---author VARCHAR,
---departmentname VARCHAR
---);
+SET MODE PostgresSQL;
+
+CREATE DATABASE departments;
+
+\c departments
+
+
+
+CREATE TABLE departments(
+id SERIAL PRIMARY KEY,
+departmentname VARCHAR,
+departmentdescription VARCHAR,
+noofemployees INT
+);
+
+CREATE TABLE users(
+id SERIAL PRIMARY KEY,
+username VARCHAR,
+userposition VARCHAR,
+userrole VARCHAR,
+departmentId INT
+);
+
+CREATE TABLE news(
+id SERIAL PRIMARY KEY,
+title VARCHAR,
+news VARCHAR,
+author VARCHAR,
+departmentId INT
+);
 
--- src/main/java/App.java
+++ src/main/java/App.java
@@ -1,2 +1,26 @@
+import com.google.gson.Gson;
+import dao.Sql2oDepartmentNewsDao;
+import dao.Sql2oDepartmentsDao;
+import dao.Sql2oGeneralNewsDao;
+import dao.Sql2oUsersDao;
+import org.sql2o.Sql2o;
+
+import java.sql.Connection;
+
 public class App {
+
+    public static void main (String[] args) {
+        Sql2oDepartmentsDao departmentsDao;
+        Sql2oDepartmentNewsDao departmentNewsDao;
+        Sql2oGeneralNewsDao generalNewsDao;
+        Sql2oUsersDao usersDao;
+        Connection conn;
+        Gson gson = new Gson();
+
+        String connectionString = "jdbc:h2:~/NewsAPI.db;INIT=RUNSCRIPT from 'classpath:db/create.sql'";
+        Sql2o sql2o = new Sql2o(connectionString, "moringa", "Nya2rango@");
+
+
+
+    }
 }
